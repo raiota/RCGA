c5d005e77b04015dd2db074e5f2bd869bb581a47
raiota <choppy.raio@gmail.com>
2020-03-19 22:07:11 +0900

    update 3/19

--- alternation.py
+++ alternation.py
@@ -5,6 +5,7 @@ Module for new generation alternation model.
 
 import numpy
 
+from .individual import *
 from .crossover import *
 
 
@@ -32,6 +33,8 @@ class JGG(object):
     random_type : enum
         Type of probability distribution which is used in generating children.
         For more details see :module:`crossover.py`.
+    pop_size : int
+        Number of population.
     parent_num : int
         Number of parents which is extracted for crossover.
     child_num : int
@@ -45,18 +48,34 @@ class JGG(object):
                  gene_min, gene_max,
                  crossover_type,
                  random_type,
-                 parent_num, child_num,
+                 pop_size, parent_num, child_num,
+                 chrom_size,
                  k=None, seed=None):
 
         numpy.random.seed(seed=seed)
 
+        self.dim = dim
         self.gene_min = gene_min
         self.gene_max = gene_max
+
+        # function for evaluation
+        self._eval = evaluation_func
 
         # function for crossover
         self.cross = cross(crossover_type=crossover_type,
                            random_type=random_type,
                            dim=dim, k=k, seed=seed)
         
+        self.pop_size = pop_size
         self.parent_num = parent_num
         self.child_num = child_num
+    
+
+    %staticmethod
+    def init_population(self):
+        self.population = Population(self.pop_size, self.gene_min, self.gene_max, self.dim)
+        return self.population
+    
+    %staticmethod
+    def eval(self, *args, **kwargs):
+        
---
+++ individual.py
@@ -0,0 +1,73 @@
+
+import random
+import numpy
+
+
+class Individual(numpy.ndarray):
+
+    fitness = None
+
+    def __new__(cls, chromosome):
+        return numpy.asarray(chromosome).view(cls)
+
+
+
+def genemake(gene_min, gene_max, size=None):
+    """
+    Function for Generating Gene
+
+    Parameters
+    ----------
+    gene_min : array-like
+    gene_max : array-like
+    size : int
+        size of chromosome (the numbers of gene)
+     
+    Returns
+    ----------
+    chromosome : array-like
+        Constituted only operated parameters
+
+    Notes
+    ----------
+    Usage below if you wanna make chromosome [gene1, gene2, gene3, gene4, ..., geneN]
+        low : [gene1_min, gene2_min, gene3_min, ..., geneN_min]
+        up : [gene1_max, gene2_max, gene3_max, ..., geneN_max]
+    """
+    try:
+        return [random.uniform(a, b) for a, b in zip(gene_min, gene_max)]
+    except TypeError:
+        return [random.uniform(a, b) for a, b in zip([gene_min] * size, [gene_max] * size)]
+
+
+class Population(list):
+
+    def __new__(cls, pop_size, gene_min, gene_max, chromo_size):
+        cls = [Individual(genemake(gene_min, gene_max, size=chromo_size)) for _ in range(pop_size)]
+        return cls
+
+
+def extract(container, num):
+    """
+    Extracting `num` values from `container`
+
+    Parameters
+    ----------
+    containter : list
+    num : int
+        Numbers for extracting
+    
+    Returns
+    ----------
+    extracted : list
+        List from extracted values, which of length is num
+    container : list
+        Extracted `container`, which of length is len(`container`) - `num`
+    """
+    random.shuffle(container)
+    extracted = []
+
+    for _ in range(num):
+        extracted.append(container.pop())
+        
+    return extracted, container
